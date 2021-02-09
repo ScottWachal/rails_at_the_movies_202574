67eeec0631f078276a008fafbea78897e90380cb
Scott Wachal <scott.wachal@gmail.com>
2021-02-09 10:44:07 -0600

    Added functionality for top 10 production companies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,18 @@ class HomeController < ApplicationController
     @movies = Movie.includes(:production_company)
                    .order("average_vote DESC")
                    .limit(10)
-    @production_companies = ProductionCompany.limit(10)
+
+    @production_companies = ProductionCompany.select("production_companies.*")
+                                             .select("COUNT(production_companies.id) as movie_count")
+                                             .joins(:movies)
+                                             .group("production_companies.id")
+                                             .order("movie_count DESC")
+                                             .limit(10)
+
+    # SELECT COUNT(*) as movie_count, production_companies.name, production_companies.id
+    # FROM production_companies
+    # JOIN movies ON production_companies.id = movies.production_company_id
+    # GROUP BY production_companies.name
+    # ORDER BY movie_count DESC;
   end
 end
