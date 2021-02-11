c65b917252ab2398a97551a1b4c8bf1cc5b6c40f
Scott Wachal <scott.wachal@gmail.com>
2021-02-11 10:35:48 -0600

    created Genre and MovieGenre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :movie_genres
+  has_many :genres, through: movie_genres
   belongs_to :production_company
   validates :title, :year, :duration, :average_vote, :description, presence: true
   validates :title, uniqueness: true
